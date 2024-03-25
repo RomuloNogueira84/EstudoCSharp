6aa8638c5c1b09ea2c7739274fd1de0b466f9ae3
Romulo Nogueira de Souza <romulonogueira929@gmail.com>
2024-03-25 14:05:40 -0300

    Add files via upload

---
+++ EstudoC#-3/RomuloProcessamentoDeDados/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace RomuloProcessamentoDeDadosCasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            x = 5;
+            y = 2 * x;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            double b, B, h, area;
+            b = 6.0;
+            B = 8.0;
+            h = 5.0;
+
+            area = (b + B) / 2.0 * h;
+            Console.WriteLine(area);
+            
+
+
+            int a, c;
+
+            double resultado;
+            double resultado1;
+            a = 5;
+            c = 2;
+
+            resultado = a / c;
+            resultado1 = (double) a / c;
+            Console.WriteLine(resultado);
+            Console.WriteLine(resultado1);
+
+
+
+
+            double g;
+            int l;
+            g = 5.0;
+            l = (int)g;
+
+            Console.WriteLine(l);
+            Console.ReadLine();
+        } 
+    }
+}
