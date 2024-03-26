12bb6bffd0d580efb98932a8ec3f4376412817e9
Romulo Nogueira de Souza <romulonogueira929@gmail.com>
2024-03-26 14:08:36 -0300

    Add files via upload

---
+++ EstudoC#-5/EntradaDeDados/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EntradaDeDados1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+            string s = Console.ReadLine();
+
+            string[] v = s.Split(' ');
+            ///string[] v = Console.ReadLine().Split(' '); tambem pode se utilizar
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
+
+            Console.WriteLine(" Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+        }
+    }
+}
